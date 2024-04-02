47a724e50ae2f4e4088710d9024fd4e07a71da20
ridhaaa <51639407+ridhaaa@users.noreply.github.com>
2024-04-02 12:15:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import AppHeader from "./components/AppHeader"
+import AppBody from "./components/AppBody"
+import HowItWorks from "./components/HowItWorks"
+import About from "./components/About"
+import Footer from "./components/Footer"
+
+
+function App(){
+  return(
+    <>
+    <AppHeader/>
+    <AppBody/>
+    <HowItWorks/>
+    <About/>
+    <Footer/>
+    </> 
+  )
+}
+
+export default App;
